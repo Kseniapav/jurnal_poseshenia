1e6a3909ee218baf9c534bc9b0eb5840c1df9158
User <test@test.ru>
2025-02-26 09:22:59 +0300

    Создание модели

---
+++ Model/Specialti.cs
@@ -0,0 +1,15 @@
+﻿namespace jurnal_poseshenia.Model
+{
+    public class Specialti
+    {
+        public int Id {  get; set; }
+
+        [Required(ErrorMessage = "Требуется название.")]
+        [StringLength(100, ErrorMessage = "Название не может быть длиннее 100 символов")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "Укажите срок обучения.")]
+        [RegularExpression(@"^(34/46)$", ErrorMessage = "Укажите 34 или 46 в поле 'Срок обучения'")] //срок указан в месяцах
+        public required int TermОfStudy { get; set; }
+    }
+}
