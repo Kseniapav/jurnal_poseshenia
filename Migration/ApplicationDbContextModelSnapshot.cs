2d8d81e747b16dc7c11691f0bd248210240cb9b2
YanaTyzik <denisyuk.yanina@mail.ru>
2025-03-27 08:58:32 +0300

    добавила папку миграции

---
+++ Migration/ApplicationDbContextModelSnapshot.cs
@@ -0,0 +1,31 @@
+﻿using jurnal_poseshenia.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace jurnal_poseshenia.Migration
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder
+                .HasAnnotation("ProductVersion", "9.0.1")
+                .HasAnnotation("Relational:MaxIdentifierLength", 128);
+
+            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
+
+            modelBuilder.Entity("jurnal_poseshenia.Model.Student", b =>
+            {
+                b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                b.Property<string>("Surname")
+
+            });
+        }
+}   }
