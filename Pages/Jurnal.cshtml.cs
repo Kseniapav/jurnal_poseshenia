a2c5d9da092167218853f2b2febf568705e90bac
Kseniapav <kseniapavlova5916@gmail.com>
2025-04-10 09:35:41 +0300

    пока только создала страницу

---
+++ Pages/Jurnal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace jurnal_poseshenia.Pages
+{
+    public class JurnalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
