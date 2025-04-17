cb7460d645cde8a3e30bc5be041e0326ff4d0163
Kseniapav <kseniapavlova5916@gmail.com>
2025-04-17 08:50:28 +0300

    Сделали миграции по божески

---
+++ Migrations/20250417054909_Migr.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace jurnal_poseshenia.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Specialtis",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    TermОfStudy = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialtis", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Surname = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Partomymic = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    SpecialtiId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Specialtis_SpecialtiId",
+                        column: x => x.SpecialtiId,
+                        principalTable: "Specialtis",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jurnals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Specialty = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    VisitDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jurnals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jurnals_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jurnals_StudentId",
+                table: "Jurnals",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_SpecialtiId",
+                table: "Students",
+                column: "SpecialtiId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jurnals");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Specialtis");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,15 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("JurnalDatabase"),
-        new MySqlServerVersion(new Version(8, 0, 23)) // Версия MySQL (8.0.23)
-    ));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("JurnalDatabase")));
 
 
-// Настройка подключения к базе данных
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-     options.UseSqlServer(builder.Configuration.GetConnectionString("JurnalDatabase")));
+
 
 var app = builder.Build();
 
