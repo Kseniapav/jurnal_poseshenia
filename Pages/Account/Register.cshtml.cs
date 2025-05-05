e8d14e064df8c97a43e0c18fa3253c0b767e8828
Kseniapav <kseniapavlova5916@gmail.com>
2025-05-05 23:24:16 +0300

    создала страницы профиля и регистрации

---
+++ Pages/Account/Register.cshtml.cs
@@ -0,0 +1,56 @@
+using jurnal_poseshenia.Data;
+using jurnal_poseshenia.Model.AuthApp;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace jurnal_poseshenia.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RegisterModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Model.AuthApp.RegisterModel Input { get; set; }
+
+        public void OnGet() { }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            bool isFirstUser = !_context.AuthUsers.Any();
+
+            var user = _context.AuthUsers.FirstOrDefault(u => u.Email == Input.Email);
+
+            if (user == null)
+            {
+                _context.AuthUsers.Add(new AuthUser { Email = Input.Email, Password = Input.Password, Role = isFirstUser ? "Admin" : "User" });
+                await _context.SaveChangesAsync();
+
+                await Authenticate(Input.Email);
+                return RedirectToPage("/Index");
+            }
+
+            ModelState.AddModelError(string.Empty, "????? ???????????? ??? ??????????");
+            return Page();
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            var claims = new List<Claim> { new Claim(ClaimsIdentity.DefaultNameClaimType, userName) };
+            var identity = new ClaimsIdentity(claims, "ApplicationCookie");
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+        }
+    }
+}
