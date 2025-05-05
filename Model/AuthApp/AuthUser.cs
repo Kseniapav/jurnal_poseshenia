48f82a07c1c5cff79e8bfb424197b508e1800b29
Kseniapav <kseniapavlova5916@gmail.com>
2025-05-05 23:49:49 +0300

    внесла коррективы

--- Model/AuthApp/AuthUser.cs
+++ Model/AuthApp/AuthUser.cs
@@ -6,6 +6,6 @@ namespace jurnal_poseshenia.Model.AuthApp
         public int Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Role { get; set; }
+        public string Role { get; set; } = "User"; // По умолчанию обычный пользователь
     }
 }
